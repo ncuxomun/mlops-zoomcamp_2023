3b55b563191b75c01b94d942771faef314b725b5
Ulugbek Djuraev <54369128+ncuxomun@users.noreply.github.com>
2023-06-11 11:24:04 -0400

    Add files via upload

---
+++ 03-orchestration/create_s3_bucket_block_UD.py
@@ -0,0 +1,23 @@
+from time import sleep
+from prefect_aws import S3Bucket, AwsCredentials
+
+
+def create_aws_creds_block():
+    my_aws_creds_obj = AwsCredentials(
+        aws_access_key_id="AKIAS5EH34VPDFJTCWSQ", aws_secret_access_key="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"
+    )
+    my_aws_creds_obj.save(name="my-aws-creds", overwrite=True)
+
+
+def create_s3_bucket_block():
+    aws_creds = AwsCredentials.load("my-aws-creds")
+    my_s3_bucket_obj = S3Bucket(
+        bucket_name="mlzoomcamp-2023", credentials=aws_creds
+    )
+    my_s3_bucket_obj.save(name="s3-bucket-example", overwrite=True)
+
+
+if __name__ == "__main__":
+    create_aws_creds_block()
+    sleep(5)
+    create_s3_bucket_block()
